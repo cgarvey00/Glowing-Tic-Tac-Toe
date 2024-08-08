176ded1a51e3193db5f4ab925cd00c597b95fcbb
cgarvey00 <conorgarvey19@outlook.com>
2024-08-08 21:58:33 +0100

    Amended the Javascript code to include combinations for checking i

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,47 @@
-
+/*----- constants -----*/
 const element=document.querySelectorAll('.game-cell');
-let player1;
-let player2;
-
 const matchOver=true;
-let turn;
+//Player One is True which is X
+//Player Two is False which is O
+let turn=true;
 
-let count;
+let count=0;
 
-let combinations;
+const combinations=[
+[1,2,3,4],
+[1,6,11,16],
+[1,5,9,13],
+[13,10,7,4],
+[2,6,10,14],
+[5,6,7,8],
+[9,10,11,12],
+[13,14,15,16],
+[3,7,11,15],
+[4,8,12,16],
+[3,7,11,15],
+];
 
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
 const grid = document.querySelectorAll('.game-cell');
 grid.forEach((box) =>{
 
 box.addEventListener("click",()=>{
 // box.innerHTML="<center><img src='images/oes.png' style='width: 80px; height: 70px;'/></center>";
-box.innerHTML="<center><h2 style='width: 180px; height: 140px; color:red;'>X</h2></center>";
+
+if(turn){
+box.innerHTML="<center><h2 style='width: 50px;  color:red; font-size:40px; '>X</h2></center>";
+count++;
+turn=false;
+}
+else if (!turn) {
+    box.innerHTML="<center><h2 style='width: 50px;  color:blue; font-size:40px; '>0</h2></center>";
+    count++;
+    turn=true;
+} 
+box.disabled=true;
 });
 });
 
